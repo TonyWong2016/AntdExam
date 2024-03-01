2b7687c5a7223808125bc7d25f7a7d970763ac78
Tony_df <wtlemon@126.com>
2024-03-01 17:32:29 +0800

    .

---
+++ app/info/create/page.tsx
@@ -0,0 +1,14 @@
+import Form from '@/app/ui/info/create-form'
+
+
+const Create = () => (
+    <div className="App">
+        <h1 className="text-green-500 font-bold">
+            TailWindCSS效果
+        </h1>
+
+
+    </div>
+);
+
+export default Create;
---
+++ app/ui/content.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+
+{/* <Breadcrumb style={{ margin: '16px 0' }}>
+<Breadcrumb.Item>Home</Breadcrumb.Item>
+<Breadcrumb.Item>List</Breadcrumb.Item>
+<Breadcrumb.Item>App</Breadcrumb.Item>
+</Breadcrumb>
+<Content
+style={{
+    padding: 24,
+    margin: 0,
+    minHeight: '80vh',
+    // background: colorBgContainer,
+    // borderRadius: borderRadiusLG,
+}}
+>
+Content
+</Content> */}
